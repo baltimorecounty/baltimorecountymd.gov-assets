9ab976d7335924558ab2fc99b392937723afe4b0
Marty Powell <jpowel25@gmail.com>
2015-06-08 10:25:09 -0400

    Removed analytics code
    Moved to their own separate files.

--- js/homepage-template.js
+++ js/homepage-template.js
@@ -31,19 +31,4 @@
         $('.primary-nav, .secondary-nav').toggleClass('mobile-menu-visible');
     });
 
-    var sendGoogleEvent = function(desc, act) {
-        act = act || 'click';
-        ga('send', 'event', 'button', act, desc);
-    };
-
-    /*Event handling*/
-    //Search Button
-    $(document).on('click', '.search-button', function() {
-        sendGoogleEvent('search-button');
-    });
-    //Search Result
-    $(document).on('click', '.gsc-table-result div.gs-title a.gs-title', function() {
-        sendGoogleEvent('search-result');
-    });
-
 })(jQuery, Flickr);
