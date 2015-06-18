c2a788e3364b80aa208aa40b173081c0c62d3824
Marty Powell <jpowel25@gmail.com>
2015-06-18 09:46:56 -0400

    Fixed issue with homepage and template files being minified

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 var gulp = require('gulp'),
 	concat = require('gulp-concat'),
 	uglify = require('gulp-uglify'),
-	rename = require('gulp-rename');
+	rename = require('gulp-rename'),
+	sass = require('gulp-sass');
 
 var concatFiles = function(files, name, dest) {
-	dest = dest || './dist/';
+	dest = dest || './dist/js';
 
 	return gulp.src(files)
 	    .pipe(concat(name))
 	    .pipe(gulp.dest(dest));
 };
+
+gulp.task('sass', function () {
+  gulp.src('./stylesheets/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'));
+});
 
 gulp.task('concatHomepageJs', function() {
 	var files = ['./js/lib/jQuery.min.js', './js/lib/slick.min.js', './js/lib/handlebars.js', './js/lib/picturefill.min.js', './js/flickr-feed.js', './js/county-news-snippet.js', './js/homepage-template.js'];
@@ -22,19 +29,16 @@ gulp.task('concatTemplateJs', function() {
 });
 
 gulp.task('compressFiles', ['concatHomepageJs', 'concatTemplateJs'], function() {
-  return gulp.src('dist/*.js')
+  return gulp.src('./dist/js/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('dist'));
+    .pipe(rename(function (path) {
+        if(path.extname === '.js') {
+            path.basename += '.min';
+        }
+    }))
+    .pipe(gulp.dest('./dist/js'));
 });
 
-gulp.task('default', ['compressFiles'], function() {
-	var homepage =  gulp.src("dist/homepage.js")
-		.pipe(rename("homepage.min.js"))
-		.pipe(gulp.dest("./dist")); 
-
-	var template = gulp.src("dist/template.js")
-		.pipe(rename("template.min.js"))
-		.pipe(gulp.dest("./dist"));
-
+gulp.task('default', ['compressFiles', 'sass'], function() {
 		return;
 });
