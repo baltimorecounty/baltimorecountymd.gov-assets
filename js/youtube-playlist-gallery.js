d31c09ffa40583cfebda9f39aa040b79af5e6e83
Michael Snyder <mxsnyder@baltimorecountymd.gov>
2016-12-16 14:56:33 -0500

    Gallery style and loadMoreButton work

--- js/youtube-playlist-gallery.js
+++ js/youtube-playlist-gallery.js
@@ -2,8 +2,14 @@ var baltimoreCounty = baltimoreCounty || {};
 
 baltimoreCounty.youtubePlaylistGallery = (function($) {
 
-    var API_KEY = 'AIzaSyBsGskkWEi-CdCx4dze-ikK2KzE7i-O450',
-        documentationLink = 'https://goo.gl/HbhJ1p';
+    var that = this;
+
+    that.API_KEY = 'AIzaSyBsGskkWEi-CdCx4dze-ikK2KzE7i-O450';
+    that.documentationLink = 'https://goo.gl/HbhJ1p';
+    that.options = {
+        target: '.bc-youtube-playlist-gallery',
+        playlistId: ''
+    };
 
     /*
      * Initializes based on supplied options, and throws errors if they're missing. 
@@ -11,29 +17,32 @@ baltimoreCounty.youtubePlaylistGallery = (function($) {
     function init(options) {
         
         if (!options) 
-            throw 'An "options" object must be supplied to the init function. Please see documentation at ' + documentationLink + '.';
+            options = that.options;
 
-        var $youtubePlaylistGalleryTarget = options.target.trim().length === 0 ?  $('.youtube-playlist-gallery') : $(options.target.trim());
-        if (!$youtubePlaylistGalleryTarget) 
-            throw 'The "target" option must be supplied. Please see documentation at ' + documentationLink + '.';
+        var $youtubePlaylistGalleryTarget = options.target ?  $(options.target.trim()) : $(that.options.target);
+        if (!$youtubePlaylistGalleryTarget.length === 0) 
+            throw 'The "target" option value must be supplied. Please see documentation at ' + documentationLink + '.';
 
-        var playlistId = options.playlistId;
+        var playlistId = options.playlistId ? options.playlistId : that.options.playlistId;
         if (!playlistId || playlistId.length === 0) 
             throw 'The "playlistId" option must be supplied. Please see documentation at ' + documentationLink + '.';
 
-        getPlaylistItems(playlistId, $youtubePlaylistGalleryTarget, generateYouTubePlaylistHtml)
+        // A little redundant, but since null isn't quite false, let's check anyway.
+        var showDescription = options.showDescription ? options.showDescription : that.options.showDescription; 
+
+        getPlaylistItems(playlistId, $youtubePlaylistGalleryTarget, showDescription, generateYouTubePlaylistHtml);
     }
 
     /*
      * Makes the requst to the YouTube v3 API.
      */
-    function getPlaylistItems(playlistId, $target, callback) {
+    function getPlaylistItems(playlistId, $target, showDescription, callback) {
         var url = 'https://www.googleapis.com/youtube/v3/playlistItems?maxResults=50&playlistId=' + playlistId + '&key=' + API_KEY + '&part=snippet',
             playlistItems = [];
 
         $.getJSON(url)
             .done(function(data) {
-                callback(data.items, $target);
+                callback(data.items, $target, showDescription);
             }) 
             .fail(function(data) {
                 console.log('Data load from YouTube failed. Response: ' + JSON.stringify(data));
@@ -43,20 +52,47 @@ baltimoreCounty.youtubePlaylistGallery = (function($) {
     /*
      * Generates the HTML for the video gallery itself. 
      */
-    function generateYouTubePlaylistHtml(playlistItems, $target) {
+    function generateYouTubePlaylistHtml(playlistItems, $target, showDescription) {
         var html = '';
 
         if (!playlistItems || playlistItems.length === 0) 
             throw 'No items are listed for the playlist.';
 
+        html += '<div class="row">';
+
         for (var i = 0; i < playlistItems.length; i++) {
-            html +=   '<div class="youtube-playlist-item">'
-                    + '    <h3>' + playlistItems[i].snippet.title + '</h3>'
-                    + '    <a href="https://www.youtube.com/watch?v=' + playlistItems[i].snippet.resourceId.videoId +  '" target="_blank"><img src="' + playlistItems[i].snippet.thumbnails.medium.url + '" alt="' + playlistItems[i].snippet.descrption + '" /></a>'
-                    + '</div>'
+            
+            if (i > 0 && i % 3 === 0)
+                if (i < 6)
+                    html += '</div><div class="row">';
+                else   
+                    html += '</div><div class="row hidden">';                
+
+            html +=   '    <div class="youtube-playlist-item">'
+                    + '        <i class="fa fa-play-circle-o" aria-hidden="true"></i>'
+                    + '        <figure>'
+                    + '            <a href="https://www.youtube.com/watch?v=' + playlistItems[i].snippet.resourceId.videoId +  '"><img src="' + playlistItems[i].snippet.thumbnails.medium.url + '" alt="' + playlistItems[i].snippet.description + '" /></a>'
+                    + '            <figcaption>'
+                    + '                <h4><a href="https://www.youtube.com/watch?v=' + playlistItems[i].snippet.resourceId.videoId +  '" target="_blank">' + playlistItems[i].snippet.title + '</a></h4>';
+
+            if (showDescription)
+                html += '                <p>' + playlistItems[i].snippet.description + '</p>';
+            
+            html +=   '            </figcaption>'
+                    + '        </figure>'
+                    + '    </div>';           
         }
 
+        html += '</div>';
+
+        html += '<button type="button" class="contentButton loadMoreButton">LOAD MORE</button>';
+
         $target.html(html);
+
+        $target.children('.loadMoreButton').first().on('click', function() {
+            $target.find('.hidden').first().removeClass('hidden');
+            $target.find('.hidden').first().removeClass('hidden');
+        });
     }
 
     return {
