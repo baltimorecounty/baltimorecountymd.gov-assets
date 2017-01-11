e94be778e0d981e73cd2c90f16b0b6d565315dc0
Michael Snyder <mxsnyder@baltimorecountymd.gov>
2017-01-11 16:40:37 -0500

    Updated cdnFallback and recompiled

--- js/utility/cdnFallback.js
+++ js/utility/cdnFallback.js
@@ -1,21 +1,41 @@
 namespacer('baltimoreCounty.utility');
 
-baltimoreCounty.utility.cdnFallback = (function($) {
+baltimoreCounty.utility.cdnFallback = (function() {
 
+    /*
+     * This is to be used after your CDN call, and will load a local script if the 
+     * CDN fails. Which never happens.
+     * 
+     * obj: The object to test for. You can pass in a string or the object itself.
+     * path: The path to the local copy of the script file.
+     * isHead: Indication of whether to load the script in the head or the body.
+     */
     var load = function(obj, path, isHead) {
-        if (!obj)
-            scriptTag(path, isHead);
+        var exists = typeof obj === 'string' ? window[obj] : obj;
+
+        if (!exists)
+            createScriptTag(path, isHead);
     },
 
-    scriptTag = function(path, isHead) {
+    /*
+     * Actual script tag creation. The tag iself will be added aboce the 
+     * closing head or body tag.
+     * 
+     * path: Path to the local copy of the script.
+     * isHead: Indication of whether to load the script in the head or the body.
+     */
+    createScriptTag = function(path, isHead) {
+        var scriptTag = document.createElement('script');
+        scriptTag.src = path;
+
         if (isHead)
-            $('head').append('<script src="' + path + '"><\/script>');
+            document.getElementsByTagName('head')[0].appendChild(scriptTag);
         else 
-            $('body').append('<script src="' + path + '"><\/script>');
+            document.getElementsByTagName('body')[0].appendChild(scriptTag);
     }
 
     return { 
         load: load
     };
 
-})(jQuery);
+})();
