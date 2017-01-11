5d936fd1c3104a985b609a8536117133cbd28569
Michael Snyder <mxsnyder@baltimorecountymd.gov>
2017-01-11 16:00:23 -0500

    Fixed gulpfile race condition

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var gulp = require('gulp'),
 	runSequence = require('run-sequence');
 
 var concatFiles = function(files, name, dest) {
-	dest = dest || './dist/js';
+	dest = dest || 'dist/js';
 
 	return gulp.src(files)
 	    .pipe(concat(name))
@@ -22,55 +22,55 @@ gulp.task('clean-dist', function() {
 });
 
 gulp.task('concatHomepageJs', function() {
-	var files = ['./js/utility/namespacer.js', 
-					'./js/utility/cdnFallback.js',
-					'./js/lib/jQuery.min.js', 
-					'./js/lib/slick.min.js', 
-					'./js/lib/handlebars.js', 
-					'./js/lib/picturefill.min.js', 
-					'./js/flickr-feed.js', 
-					'./js/county-news-snippet.js',
-					'./js/homepage-template.js'];
+	var files = ['js/utility/namespacer.js', 
+					'js/utility/cdnFallback.js',
+					'js/lib/jQuery.min.js', 
+					'js/lib/slick.min.js', 
+					'js/lib/handlebars.js', 
+					'js/lib/picturefill.min.js', 
+					'js/flickr-feed.js', 
+					'js/county-news-snippet.js',
+					'js/homepage-template.js'];
 	return concatFiles(files, 'homepage.js');
 });
 
 gulp.task('concatTemplateJs', function() {
-	var files = ['./js/polyfills/array.some.js',
-					'./js/utility/namespacer.js', 
-					'./js/utility/cdnFallback.js',
-					'./js/nifty-forms.js',
-					'./js/lib/bootstrap-collapse.js',
-					'./js/skip-nav.js',
-					'./js/text-resizer.js', 
-					'./js/bc-google-analytics.js', 
-					'./js/bc-google-analytics-custom-events.js', 
-					'./js/lib/review.js', 
-					'./js/mobile-search.js',
-					'./js/template-events.js', 
-					'./js/inside-template.js',
-					'./js/bc-content-filter.js', 
-					'./js/accordion-menu.js'];
+	var files = ['js/polyfills/array.some.js',
+					'js/utility/namespacer.js', 
+					'js/utility/cdnFallback.js',
+					'js/nifty-forms.js',
+					'js/lib/bootstrap-collapse.js',
+					'js/skip-nav.js',
+					'js/text-resizer.js', 
+					'js/bc-google-analytics.js', 
+					'js/bc-google-analytics-custom-events.js', 
+					'js/lib/review.js', 
+					'js/mobile-search.js',
+					'js/template-events.js', 
+					'js/inside-template.js',
+					'js/bc-content-filter.js', 
+					'js/accordion-menu.js'];
   	return concatFiles(files, 'template.js');
 });
 
 gulp.task('concatFormsJs', function() {
-	var files = ['./js/lib/moment.min.js',
-					'./js/lib/validate.min.js',
-					'./js/inline-form-validation.js'];
+	var files = ['js/lib/moment.min.js',
+					'js/lib/validate.min.js',
+					'js/inline-form-validation.js'];
 	return concatFiles(files, 'forms.js');
 });
 
 gulp.task('compressFiles', ['concatHomepageJs', 'concatTemplateJs', 'concatFormsJs'], function() {
-	return gulp.src(['!./dist/js/*min.js', './dist/js/*.js'])
+	return gulp.src(['dist/js/*.js'])
 		.pipe(uglify())
 		.pipe(rename({
 			suffix: '.min'
 		}))
-		.pipe(gulp.dest('./dist/js'));
+		.pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('sassAndCompressCss', function () {
-	return gulp.src('./stylesheets/*.scss')
+	return gulp.src('stylesheets/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(cssnano())
 		.pipe(rename({ suffix: '.min' }))
@@ -79,7 +79,7 @@ gulp.task('sassAndCompressCss', function () {
 
 gulp.task('watch', function() {
 	gulp.watch(['js/*.js', 'js/lib/*.js', 'js/page-specific/*.js'], ['compressFiles']);
-	gulp.watch(['./stylesheets/*.scss'], ['sassAndCompressCss']);
+	gulp.watch(['stylesheets/*.scss'], ['sassAndCompressCss']);
 });
 
 gulp.task('linter', function() {
@@ -88,6 +88,6 @@ gulp.task('linter', function() {
 		.pipe(jshint.reporter('default'));
 });
 
-gulp.task('default', function(callback) {
-	runSequence('clean-dist', ['compressFiles', 'sassAndCompressCss'], callback);
+gulp.task('default', ['clean-dist'], function(callback) {
+	return runSequence(['compressFiles', 'sassAndCompressCss'], callback);
 });
