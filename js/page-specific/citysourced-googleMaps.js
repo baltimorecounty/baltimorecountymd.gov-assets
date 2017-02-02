5658760fd7a929d8bd4628f683f35e8bb3d2c94f
Michael Snyder <mxsnyder@baltimorecountymd.gov>
2017-02-02 15:29:44 -0500

    Moved around the centering when autocomplete fires

--- js/page-specific/citysourced-googleMaps.js
+++ js/page-specific/citysourced-googleMaps.js
@@ -67,13 +67,12 @@ baltimoreCounty.pageSpecific.googleMaps = (function(googleMaps, undefined) {
 
         clearMarker();
 
-        window.map.panTo(center);
-                        
+        window.map.panTo(center);                            
+        window.map.setZoom(16);
+
         var center = window.map.getCenter();
         google.maps.event.trigger(window.map, 'resize');
         map.setCenter(center);
-        
-        window.map.setZoom(16);
 
         createMarker(latitude, longitude);
         trackLatLng(latitude, longitude);
