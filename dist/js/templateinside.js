3b2e65e5d84f37199666a2f724dd4c4b29637838
Michael Snyder <mxsnyder@baltimorecountymd.gov>
2017-05-04 13:36:17 -0400

    Fixed the selinks to be real links

--- dist/js/templateinside.js
+++ dist/js/templateinside.js
@@ -5690,18 +5690,37 @@ namespacer('baltimoreCounty');
 
 baltimoreCounty.internalCarousel = (function($) {
 
-	var init = function() {
-		$('#mainContent .carousel').slick({ 
-			autoplay: true,
-			adaptiveHeight: true,
-			autoplaySpeed: 5000,
-			dots: true,
-			/*Show dot navigation*/
-			nextArrow: "<img src='//baltimorecountymd.gov/sebin/v/i/carousel-arrow-right.png' class='slick-next' />",
-			/*Starts on slide 4*/
-			prevArrow: "<img src='//baltimorecountymd.gov/sebin/f/m/carousel-arrow-left.png 'class='slick-prev' />"
-		});
-	};
+	var fixLinks = function() {
+			var $links = $('.carousel selink');
+
+			$links.each(function(index, selink) {
+				var $selink = $(selink),
+					$newLink = $('<a>').attr('href', $selink.attr('externallink')).attr('title', $selink.attr('title')).text($selink.text());
+
+				if ($selink.attr('newwindow') && $selink.attr('newwindow') != 'false')
+					$newLink.attr('target', '_blank');
+
+				$selink.after($newLink);
+				$selink.detach();
+			});
+
+		},
+	
+		init = function() {
+			fixLinks();
+
+			$('#mainContent .carousel').slick({ 
+				autoplay: true,
+				adaptiveHeight: true,
+				autoplaySpeed: 5000,
+				lazyLoad: true,
+				dots: true,
+				/*Show dot navigation*/
+				nextArrow: "<img src='//baltimorecountymd.gov/sebin/v/i/carousel-arrow-right.png' class='slick-next' />",
+				/*Starts on slide 4*/
+				prevArrow: "<img src='//baltimorecountymd.gov/sebin/f/m/carousel-arrow-left.png 'class='slick-prev' />"
+			});
+		};
 
 	return {
 		init: init
