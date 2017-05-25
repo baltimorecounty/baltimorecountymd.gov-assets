85d8b08ac3bc3c3a1e3c284e76454c86a5fc0a15
Marty Powell <jpowel25@gmail.com>
2017-05-25 11:54:35 -0400

    Updated debounce method so it could be triggered properly.

--- js/utility/debounce.js
+++ js/utility/debounce.js
@@ -1,13 +1,9 @@
 namespacer('baltimoreCounty.utility');
 
-baltimoreCounty.utility.debounce = (function() {
-    var debounce = (function(){
-        var timer = 0;
-        return function(callback, ms){
-            clearTimeout (timer);
-            timer = setTimeout(callback, ms);
-        };
-    })();
-
-    return debounce;
-});
+baltimoreCounty.utility.debounce = (function(callback, ms) {
+    var timer = 0;
+    return function(callback, ms) {
+        clearTimeout(timer);
+        timer = setTimeout(callback, ms);
+    };
+})();
