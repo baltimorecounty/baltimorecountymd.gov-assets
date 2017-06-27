0a269c822bd8db398a64cd75d33faeb0ac6a2c90
Michael Snyder <mxsnyder@baltimorecountymd.gov>
2017-06-27 11:11:15 -0400

    Moved in Fastrack files, can now go to citysourced, accela, or FT

--- dist/js/page-specific/fasTrack.js
+++ dist/js/page-specific/fasTrack.js
@@ -1,111 +1,102 @@
 namespacer('baltimoreCounty.pageSpecific');
 
-baltimoreCounty.pageSpecific.fasTrack = (function($, undefined) {
+baltimoreCounty.pageSpecific.fasTrack = (function ($, undefined) {
 	'use strict';
 
-	var self = this,
-	
-		/**
-		 * Determines based on ID format if this is a FasTrack ID.
-		 */
-		isFasTrack = function(input) {
-			var fasTrackRegex = /^ex\d+$/i;
-			return fasTrackRegex.test(input);
-		},
+	var trackingCodeTests = [{
+		type: 'accela',
+		pattern: /^(ACCMP|CC|CRH|CS|PP|TS|CE|CP|CB|CG)\d+$/i,
+		action: function(trackingNumber, $form) { 
+			window.location = 'https://citizenaccess.baltimorecountymd.gov/CitizenAccess/';
+		}
+	}, {
+		type: 'baltcogo',
+		pattern: /^\d+$/i,
+		action: function(trackingNumber, $form) {
+			window.location = '/_test/baltcogo/followup-jq?reportId=' + trackingNumber;
+		}
+	}, {
+		type: 'fastrack',
+		pattern: /^ex\d+$/i,
+		action: function(trackingNumber, $form) {
+			submitToFasTrack($form);
+		}
+	}];
 
-		/**
-		 * Determined based on ID format if this is a CitySourced ID.
-		 */
-		isCitySourced = function(input) {		
-			var citySourcedRegex = /^\d+$/;
-			return citySourcedRegex.test(input);
-		},
+	/**
+	 * Submits form data to FasTrack.
+	 */
+	var submitToFasTrack = function ($form) {
+		//var _appServer = 'http://localhost:50851/ECMS/',
+		//var _appServer = 'http://dprmmstr-dev.co.ba.md.us/ECMS/',
+		//var _appServer = 'http://dprmmaster.co.ba.md.us/ECMS/',
+		var _appServer = 'http://egov.baltimorecountymd.gov/ECMS/',
+			dataString = $form.serialize();
 
-		/**
-		 * Submits form data to FasTrack.
-		 */
-		submitToFasTrack = function($form) {
-			//var _appServer = 'http://localhost:50851/ECMS/',
-			//var _appServer = 'http://dprmmstr-dev.co.ba.md.us/ECMS/',
-			//var _appServer = 'http://dprmmaster.co.ba.md.us/ECMS/',
-			var _appServer = 'http://egov.baltimorecountymd.gov/ECMS/',
-				dataString = $form.serialize();
-
-			$.ajax({
+		$.ajax({
 				url: _appServer + 'Intake/Login',
 				data: dataString,
 				type: "GET",
 				dataType: "jsonp",
 				async: false
 			})
-			.done(function(data) {
+			.done(function (data) {
 				formatJsonpResult(data);
 			})
-			.fail(function(error) {
-
+			.fail(function (error) {
+				console.log(error);
 			});
-		},
+	};
 
-		/**
-		 * Formats the response from the AJAX call. Move to promise to avoid race condition?
-		 */
-		formatJsonpResult = function(jsonpResult) {
-			var errorCounter = 0;
+	/**
+	 * Formats the response from the AJAX call. Move to promise to avoid race condition?
+	 */
+	var formatJsonpResult = function (jsonpResult) {
+		var errorCounter = 0;
 
-			if (jsonpResult.ResponseStatus == 1) {
-				var url = 'getstatus.html?correspondenceId=' + jsonpResult.ResponseError;
-				$(window.location).attr('href', url);
+		if (jsonpResult.ResponseStatus == 1) {
+			var url = 'getstatus.html?correspondenceId=' + jsonpResult.ResponseError;
+			$(window.location).attr('href', url);
+		} else {
+			// bug # 2127
+			// pop-up instead of a red error
+			//$('#errorDiv').html(jsonpResult.ResponseError);
+			// not sure why but this is getting called twice,
+			// there is a bug I think in jQuery for this.  For now work-around is set counter to 2
+			var errorMsg = "";
+			if (errorCounter == 0) {
+				errorMsg = "The information you have entered is incorrect. Please verify that the " +
+					" tracking number and email address you have entered are correct.";
+				errorCounter += 1;
+				alert(errorMsg);
+
 			} else {
-				// bug # 2127
-				// pop-up instead of a red error
-				//$('#errorDiv').html(jsonpResult.ResponseError);
-				// not sure why but this is getting called twice,
-				// there is a bug I think in jQuery for this.  For now work-around is set counter to 2
-				var errorMsg = "";            
-				if (errorCounter == 0) {
-					errorMsg = "The information you have entered is incorrect. Please verify that the " +
-						" tracking number and email address you have entered are correct.";
-					errorCounter += 1;
-					alert(errorMsg);
-					
-				}
-				else {
-					errorMsg = " The information you have entered is incorrect. Please call" +
-						" 410-887-2450 to verify your tracking number."
-					alert(errorMsg);
-				}
+				errorMsg = " The information you have entered is incorrect. Please call" +
+					" 410-887-2450 to verify your tracking number."
+				alert(errorMsg);
 			}
-		},
+		}
+	};
 
-		testAndProceed = function(testFunction, testParameter, successCallback, failureCallback) {		
-			if (testFunction(testParameter))
-				successCallback();
-			else 
-				failureCallback();
-		},
+	/**
+	 * The "submit" event handler for the "Track Now" button.
+	 */
+	var submitHandler = function (event) {
+		var $form = $('#FasTrack'),
+			$TrackingNumber = $('#TrackingNumber'),
+			trackingNumber = $TrackingNumber.val(),
+			isSuccess = false;
 
-		/**
-		 * The "submit" event handler for the "Track Now" button.
-		 */
-		submitHandler = function(event) {
-			var $form = $('#FasTrack'),
-				$TrackingNumber = $('#TrackingNumber'),
-				trackingNumber = $TrackingNumber.val();
+		$.each(trackingCodeTests, function(index, trackingCodeTest) {
+			if (trackingCodeTest.pattern.test(trackingNumber)) {
+				isSuccess = true;
+				trackingCodeTest.action(trackingNumber, $form);
+			}
+		});
 
-			testAndProceed(isCitySourced, trackingNumber, 
-				function() {
-					window.location = '/citysourced/preview/viewer?reportId=' + trackingNumber;
-				},
-				function() {
-					testAndProceed(isFasTrack, trackingNumber, 
-						function() {
-							submitToFasTrack($form);
-						},
-						function() {
-							alert("We're having trouble looking up this record. Please call 410-887-2450 to verify your tracking number.");
-						});
-				});
-		};
+		if (!isSuccess) 
+			alert("We're having trouble looking up this record. Please call 410-887-2450 to verify your tracking number.");
+	};
 
 	return {
 		submitHandler: submitHandler
@@ -113,6 +104,6 @@ baltimoreCounty.pageSpecific.fasTrack = (function($, undefined) {
 
 })(jQuery);
 
-$(function() {
+$(function () {
 	$('#Submit').on('click', baltimoreCounty.pageSpecific.fasTrack.submitHandler);
 });
