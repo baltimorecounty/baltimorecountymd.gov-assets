2cbabee714d433ba7f216922435c0f4c7bd8d667
Michael Snyder <mxsnyder@baltimorecountymd.gov>
2017-06-28 15:02:36 -0400

    Swiftype initial setup

---
+++ js/swiftype-config.js
@@ -0,0 +1,28 @@
+namespacer('baltimoreCounty');
+
+baltimoreCounty.swifTypeConfig = (function($, undefined) {
+
+	var customRenderFunction = function(document_type, item) {
+		console.log(item);
+
+		var highlight = item.highlight.sections || item.highlight.title;
+		var out = '';
+		out = out.concat('<div class="st-autocomplete-search-result">');
+		out = out.concat('<a href="' + item['url'] + '">' + item['title'] + '<br/><span>' + highlight + '</span></a>');
+		out = out.concat('</div>');
+		return out;
+	};
+
+	return {
+		customRenderFunction: customRenderFunction
+	};
+
+})(jQuery);
+
+
+$(function() { 
+	$('#search-input').swiftype({ 
+		renderFunction: baltimoreCounty.swifTypeConfig.customRenderFunction,
+		engineKey: '4nioYNESzQAAvy7fKxeY'
+	});
+});
