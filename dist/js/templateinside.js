3cd1981a5f84d8f19810d8002d75e509504037a2
Marty Powell <jpowel25@gmail.com>
2017-07-06 09:07:21 -0400

    Rebuild

--- dist/js/templateinside.js
+++ dist/js/templateinside.js
@@ -151,11 +151,19 @@ baltimoreCounty.utility.cdnFallback = (function() {
 })();
 namespacer('baltimoreCounty.utility');
 
-baltimoreCounty.utility.debounce = (function(callback, ms) {
-    var timer = 0;
-    return function(callback, ms) {
-        clearTimeout(timer);
-        timer = setTimeout(callback, ms);
+/* from https://davidwalsh.name/javascript-debounce-function */
+baltimoreCounty.utility.debounce = (function(func, wait, immediate) {
+    var timeout;
+    return function(func, wait, immediate) {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
     };
 })();
 namespacer('baltimoreCounty.utility');
