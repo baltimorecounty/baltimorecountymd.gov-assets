20240528992d04fb86d2c57fe79c5fb8048cae73
Marty Powell <jpowel25@gmail.com>
2017-07-11 11:06:32 -0400

    Added dataprovider configuration

--- js/page-specific/baltcogo-app/app.js
+++ js/page-specific/baltcogo-app/app.js
@@ -1,5 +1,15 @@
-(function() {
-	'use strict';
+(function () {
+  'use strict';
 
-	angular.module('baltcogoApp', []);
+  angular.module('baltcogoApp', [])
+    .config(["dataProvider", function (dataProvider) {
+      var urls = [
+        { "animal.breeds": '/mockups/citysourced/animal-breeds.json' },
+        { "animal.colors": '/mockups/citysourced/animal-colors.json' },
+        { "animal.types": '/mockups/citysourced/animal-types.json' },
+        { "categories": '/mockups/citysourced/categories.json' },
+        { "pet.types": '/mockups/citysourced/pet-types.json' }
+      ];
+      dataProvider.setUrls(urls);
+    }]);
 })();
