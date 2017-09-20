d20301ee64e0b01592a0ec46feb8cec2ca7ed23b
Marty Powell <jpowel25@gmail.com>
2017-09-20 12:02:00 -0400

    added validator logic and tests

---
+++ js/utility/validators.js
@@ -0,0 +1,27 @@
+namespacer('baltimoreCounty.utility');
+
+baltimoreCounty.utility.validate = (function () {
+    var _validators = {
+        phoneNumber: function (str) {
+            /**
+             * Valid Formats:
+                (123)456-7890
+                (123) 456-7890
+                123-456-7890
+                123.456.7890
+                1234567890
+                +31636363634 (not working)
+                075-63546725 (not working)
+            */
+            //var exp = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g;
+            var exp = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
+            return exp.test(str);
+        }
+    }
+
+    return function validate(key, val) {
+        return _validators[key](val);
+    };
+})();
+
+module.exports = validators;
