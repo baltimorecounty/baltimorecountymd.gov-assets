9b2f188b3f05b19551fd0d2fbc240ce5f7bbb19b
Marty Powell <jpowel25@gmail.com>
2017-09-21 10:13:30 -0400

    reorg code

--- js/utility/validators.js
+++ js/utility/validators.js
@@ -1,9 +1,8 @@
 namespacer('baltimoreCounty.utility');
 
 baltimoreCounty.utility.validate = (function () {
-    var _validators = {
-        phoneNumber: function (str) {
-            /**
+    function validatePhoneNumber(str) {
+        /**
              * Valid Formats:
                 (123)456-7890
                 (123) 456-7890
@@ -13,15 +12,20 @@ baltimoreCounty.utility.validate = (function () {
                 +31636363634 (not working)
                 075-63546725 (not working)
             */
-            //var exp = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g;
-            var exp = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
-            return exp.test(str);
-        }
+        //var exp = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g;
+        var exp = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
+        return exp.test(str);
     }
 
-    return function validate(key, val) {
-        return _validators[key](val);
+    var _validators = {
+        phoneNumber: validatePhoneNumber
     };
+
+    function validate(key, val) {
+        return _validators[key](val);
+    }
+
+    return validate;
 })();
 
 module.exports = validators;
