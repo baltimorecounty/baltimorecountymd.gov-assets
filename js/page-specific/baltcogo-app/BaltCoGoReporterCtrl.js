f8ecfa142e52b742ff59d1b78c594ada2f5d2d5e
Marty Powell <jpowel25@gmail.com>
2017-12-01 13:41:30 -0500

    changed is animal check to use id

--- js/page-specific/baltcogo-app/BaltCoGoReporterCtrl.js
+++ js/page-specific/baltcogo-app/BaltCoGoReporterCtrl.js
@@ -12,6 +12,7 @@
 		var self = this;
 		var categoryId = querystringer.getAsDictionary().categoryid * 1;
 		var map;
+		var ANIMALS_CATEGORY_ID = 1010169;
 
 		self.isAnimal = false;
 		self.page = 1;
@@ -226,7 +227,7 @@
 						self.zipCodeId = element.fields.zipCode;
 					}
 
-					self.isAnimal = element.name.toLowerCase() === 'pets and animals';
+					self.isAnimal = element.id === ANIMALS_CATEGORY_ID;
 
 					$timeout(function descriptionSettingWrapper() {
 						if (element.descriptionOfAnimal) {
