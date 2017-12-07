ccbb2c502339fe35a67cbda7c066c2591694574f
Marty Powell <jpowel25@gmail.com>
2017-12-07 10:12:30 -0500

    rebuild

--- dist/js/homepage.js
+++ dist/js/homepage.js
@@ -5674,38 +5674,3 @@ var ShowNews = (function($) {
     window.onload = deferImages;
 
 })(jQuery, Flickr);
-
-(function TemplateEvents($, TextResizer) {
-	function onDocumentReady() {
-		var textResizer = new TextResizer({
-			listClass: 'resizer-list'
-		});
-	}
-
-	function searchButtonClicked(e) {
-		var val = $('.search-input').val();
-
-		if (val.length === 0) {
-			e.preventDefault();
-		}
-	}
-
-	function toggleMobileNavigation(e) {
-		e.preventDefault();
-		$('.primary-nav, .secondary-nav').toggleClass('mobile-menu-visible');
-	}
-
-	/**
-   * Stuff to kick off when the template is loaded
-   */
-	$(document).ready(onDocumentReady);
-
-	/**
-   * Events
-   */
-	/* Toggle hamburger menu */
-	$(document).on('click', '.hamburger-btn', toggleMobileNavigation);
-
-	/* Prevent a search with no text */
-	$(document).on('click', '.search-button', searchButtonClicked);
-}(jQuery, TextResizer));
